d8e1f80501b14a8d6f791726644e86f8dbeb2e54
Shadi Abdelslam <shadodev@gmail.com>
2024-01-09 01:13:03 +0200

    Create Hero Section
    - Create Hero Section using Hyper UI
    - Change Font in Next.js 14
    - Enhance Header component

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 function Header() {
   return (
     <header className="bg-white dark:bg-gray-900">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-md">
-        <Image src='/logo.svg' alt='logo' width={150} height={100} />
+      <div className="mx-auto flex h-16 items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-md">
+        <Image src="/logo.svg" alt="logo" width={150} height={100} />
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block">
@@ -67,7 +67,7 @@ function Header() {
               </a>
 
               <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-500 transition hover:text-teal-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75 sm:block"
+                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-primary transition hover:text-teal-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75 sm:block"
                 href="/"
               >
                 Register
--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import Hero from './_components/Hero'
 
 export default function Home() {
   return (
-    <div>
-      Let`s Start
-    </div>
+    <div><Hero /></div>
   )
 }
