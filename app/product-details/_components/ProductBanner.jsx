462f7da8981c1cd14abdca7d058ccba3d2c16512
Shadi Abdelslam <shadodev@gmail.com>
2024-01-15 07:54:34 +0200

    Add Skeleton Loading Effect
    - Create SkeletonProductInfo Component for Product Details
    - Use SkeletonProductInfo Component in ProductInfo

--- app/product-details/_components/ProductBanner.jsx
+++ app/product-details/_components/ProductBanner.jsx
@@ -4,15 +4,19 @@ import React from 'react'
 function ProductBanner({ product}) {
   return (
     <div>
-        <Image 
-            src={product?.attributes?.banner?.data?.attributes?.url}
-            alt={product?.attributes?.title}
-            width={400}
-            height={400}
-            className='rounded-lg'
+      {product?.attributes?.banner?.data?.attributes?.url ? (
+        <Image
+          src={product?.attributes?.banner?.data?.attributes?.url}
+          alt={product?.attributes?.title}
+          width={400}
+          height={400}
+          className="rounded-lg"
         />
+      ) : (
+        <div className="w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse"></div>
+      )}
     </div>
-  )
+  );
 }
 
 export default ProductBanner
--- app/product-details/_components/ProductInfo.jsx
+++ app/product-details/_components/ProductInfo.jsx
@@ -1,26 +1,38 @@
+import SkeletonProductInfo from '@/app/product-details/_components/SkeletonProductInfo';
 import { AlertOctagon, BadgeCheck, ShoppingCart } from 'lucide-react';
 import React from 'react'
 
 function ProductInfo({product}) {
   return (
     <div>
-      <h2 className="text-[20px]">{product?.attributes?.title}</h2>
-      <h2 className="text-[15px] text-gray-400">
-        {product?.attributes?.category}
-      </h2>
-      <h2 className="text-[11px] mt-5">
-        {product?.attributes?.description[0]?.children[0]?.text}
-      </h2>
-      <h2 className="flex gap-2 items-center mt-2 text-[11px] text-gray-500">
-        {product?.attributes?.instantDelivery ? <BadgeCheck className='text-green-500'/>: <AlertOctagon className='text-yellow-500'/>} Eligible For Instant Delivery
-      </h2>
-      <h2 className="text-[32px] text-primary mt-3">
-        {product?.attributes?.price}
-      </h2>
-      <button className="flex gap-2 bg-primary text-white hover:bg-teal-600 py-2 px-4 rounded-md mt-5">
-        <ShoppingCart />
-        Add to cart
-      </button>
+        {product?.id ?
+        <div>
+        <h2 className="text-[20px]">{product?.attributes?.title}</h2>
+        <h2 className="text-[15px] text-gray-400">
+          {product?.attributes?.category}
+        </h2>
+        <h2 className="text-[11px] mt-5">
+          {product?.attributes?.description[0]?.children[0]?.text}
+        </h2>
+        <h2 className="flex gap-2 items-center mt-2 text-[11px] text-gray-500">
+          {product?.attributes?.instantDelivery ? (
+            <BadgeCheck className="text-green-500" />
+          ) : (
+            <AlertOctagon className="text-yellow-500" />
+          )}{" "}
+          Eligible For Instant Delivery
+        </h2>
+        <h2 className="text-[32px] text-primary mt-3">
+          {product?.attributes?.price}
+        </h2>
+        <button className="flex gap-2 bg-primary text-white hover:bg-teal-600 py-2 px-4 rounded-md mt-5">
+          <ShoppingCart />
+          Add to cart
+        </button>
+      </div>  
+        :
+        <SkeletonProductInfo />
+        }
     </div>
   );
 }
